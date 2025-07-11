871fb36f6d4e75b5896f783158ef103d746673fa
Maxim Vorobev-Kovrov <vorobevkovrov@gmail.com>
2025-07-11 17:30:50 +0300

    some code added

---
+++ src/main/java/ru/vorobev/openweathermapbot/service/impl/UrlBuilderImpl.java
@@ -0,0 +1,34 @@
+package ru.vorobev.openweathermapbot.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.vorobev.openweathermapbot.configuration.OpenWeatherMapConfig;
+import ru.vorobev.openweathermapbot.entity.weather.TemperatureUnits;
+import ru.vorobev.openweathermapbot.service.UrlBuilder;
+
+@Service
+public class UrlBuilderImpl implements UrlBuilder {
+    private OpenWeatherMapConfig weatherMapConfig;
+    private final static Logger LOG = LoggerFactory.getLogger(CurrentWeatherDataImpl.class);
+    private TemperatureUnits temperatureUnits;
+
+    public UrlBuilderImpl(OpenWeatherMapConfig weatherMapConfig) {
+        this.weatherMapConfig = weatherMapConfig;
+    }
+
+    public String buildWeatherUrl(double lat, double lon) {
+        LOG.info(" private String buildWeatherUrl");
+        return UriComponentsBuilder.fromUriString(weatherMapConfig.getAPIUrl())
+                .queryParam("lat", lat)
+                .queryParam("lon", lon)
+                .queryParam("appid", weatherMapConfig.getAPIkey())
+                .queryParam("units", "metric")
+                .queryParam("lang", "ru")
+                .build()
+                .toUriString();
+    }
+
+}
